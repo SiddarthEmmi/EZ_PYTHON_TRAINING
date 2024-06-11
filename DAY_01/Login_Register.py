c9eb4e26a2e5724634d935fcd7fa1f61af7c321c
SIDDARTHA_EMMI <118586112+SiddarthEmmi@users.noreply.github.com>
2024-06-11 20:56:05 +0530

    Update Login_Register.py

--- DAY_01/Login_Register.py
+++ DAY_01/Login_Register.py
@@ -1,5 +1,11 @@
 
 #DAY-01
+'''
+Write a program to build login system using functions.The function name should be login_register
+1)It should ask user to enter the details for registers and out of all details only username and password should be stored
+2)Now this function should ask user to enter username and password.If these match with the registered details login successfull 
+otherwise repeat this login process by saying that invalid creditionals until they are right.
+'''
 def login_register():
     d={}
     print("Welcome to registration")
